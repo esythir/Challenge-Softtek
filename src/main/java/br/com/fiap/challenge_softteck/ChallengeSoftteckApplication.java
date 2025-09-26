a84ed3657366621688af1604ca40fb9557f23b7a
Dias <guidiaspf@gmail.com>
2025-09-26 10:31:12 -0300

    chore: update dependencies and configuration for Firebase migration

--- src/main/java/br/com/fiap/challenge_softteck/ChallengeSoftteckApplication.java
+++ src/main/java/br/com/fiap/challenge_softteck/ChallengeSoftteckApplication.java
@@ -2,8 +2,17 @@ package br.com.fiap.challenge_softteck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"br.com.fiap.challenge_softteck.domain",
+		"br.com.fiap.challenge_softteck.dto",
+		"br.com.fiap.challenge_softteck.framework",
+		"br.com.fiap.challenge_softteck.interfaceadapter",
+		"br.com.fiap.challenge_softteck.port",
+		"br.com.fiap.challenge_softteck.usecase"
+})
 public class ChallengeSoftteckApplication {
 
 	public static void main(String[] args) {
